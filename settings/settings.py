dc25720cf68992a9e1574b9f50ada2f298cfdf7d
Milan Miladinovic <milanmiladinovic@outlook.com>
2018-09-09 12:23:37 -0400

    Changed variable "ret" to "settings" because ret is meaningless.

--- settings/settings.py
+++ settings/settings.py
@@ -21,18 +21,18 @@ class Settings:
     def __init__(self):
         try:
             with open("settings/settings.txt", 'r') as f:
-                ret = f.read()
-                ret = ret.split('\n')
+                settings = f.read()
+                settings = settings.split('\n')
 
-            self.fiat = str(ret[0])
-            if ret[1] == "hourly" or ret[1] == "daily" or ret[1] == "weekly":
-                self.time_frame = ret[1]
+            self.fiat = str(settings[0])
+            if settings[1] == "hourly" or settings[1] == "daily" or settings[1] == "weekly":
+                self.time_frame = settings[1]
             else:
                 raise ValueError("settings/settings.txt line 2 must be one of "
                                  + "(hourly, daily, weekly)! Default: daily"
                                  + "price change.")
             # must be an integer
-            self.update = ceil(int(ret[2])) * 60
+            self.update = ceil(int(settings[2])) * 60
         except ValueError:
             # defaults to USD
             self.fiat = "USD"
